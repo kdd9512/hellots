76d8064dd6a0b3cef06c73bb8c829d6d5144baad
kdd9512 <hyunwoo8805@gmail.com>
2021-06-10 13:47:53 +0900

    20210610 commit04
    React Hooks 02

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {Component, useState} from 'react';
 import {createGlobalStyle} from 'styled-components';
 import Number from "./Number";
 import {Form, Input} from "./Input";
+import {useInput} from "./useInput";
 
 // interface IState {
 //     counter: number
@@ -44,40 +45,66 @@ import {Form, Input} from "./Input";
 //     }
 // }
 
-const useInput = (initVal:any, validator:any) => {
-    const [value, setValue] = useState(initVal);
-    const onChange = (e: { target: { value:any } }) => {
-        const {
-            target: {value}
-        } = e;
-        let isUpdate = true;
-        if (typeof validator === "function") {
-            isUpdate = validator(value);
-        }
-        if (isUpdate) {
-            setValue(value)
-            console.log(value)
-        }
+// const useInput = (initVal:any, validator:any) => {
+//     const [value, setValue] = useState(initVal);
+//     const onChange = (e: { target: { value:any } }) => {
+//         const {
+//             target: {value}
+//         } = e;
+//         let isUpdate = true;
+//         if (typeof validator === "function") {
+//             isUpdate = validator(value);
+//         }
+//         if (isUpdate) {
+//             setValue(value)
+//             console.log(value)
+//         }
+//     }
+//     return { value, onChange };
+// }
+
+const content = [
+    {
+        tab: "Section 1",
+        content: "I'm the content of the Section 1"
+    },
+    {
+        tab: "Section 2",
+        content: "I'm the content of the Section 2"
     }
-    return { value, onChange };
+]
+
+const useTabs = (initTab: any, allTabs: any) => {
+    const [currentIndex, setCurrentIndex] = useState(initTab)
+
+    if (!allTabs || !Array.isArray(allTabs)) {
+        return;
+    }
+    return {
+        currentItem: allTabs[currentIndex],
+        changeItem: setCurrentIndex
+    }
 }
 
 const App = () => {
-    {/*const [count, setCount] = useState(0);*/}
+    //
+    // const [count, setCount] = useState(0);
     // const [email, setEmail] = useState("");
     //
-    {/*const updateEmail = (e: { target: { value: string; }; }) => {*/}
-    {/*    const {*/}
-    {/*        target: {value}*/}
+    // const updateEmail = (e: { target: { value: string; }; }) => {
+    //     const {
+    //         target: {value}
     //     } = e;
-    {/*    setEmail(value);*/}
-    {/*    console.log(value);*/}
-    {/*};*/}
-    const maxlength = (value:any) => value.length <= 10 &&
-        !value.includes("@") && !value.includes("#");
-        // ,"$","%","^","&","*","(",")","_","+","-","=","`","~","!",",","<",".",">",";",":","'","\"","\\","|","[","]","{","}"
-    const name = useInput("Mr.",maxlength)
+    //
+    //     setEmail(value);
+    //     console.log(value);
+    // };
+    //
+    // const maxlength = (value: any) => value.length <= 10 &&
+    //     !value.includes("@") && !value.includes("#");
+    // const name = useInput("Mr.", maxlength)
 
+    const { currentItem, changeItem }: any = useTabs(0, content);
 
     return (
         <div className="App">
@@ -86,7 +113,13 @@ const App = () => {
             {/*<button onClick={() => setCount(count - 1)}> - </button>*/}
             {/*<input placeholder="email" value={email} onChange={updateEmail}/>*/}
 
-            <input placeholder="Name" {...name} />
+            {/*<input placeholder="Name" {...name} />*/}
+
+            {content.map(
+                (section ,index) => (
+                    <button onClick={() => changeItem(index)}>{section.tab}</button>
+                ))}
+            <div>{currentItem.content}</div>
         </div>
     );
 }
--- src/useInput.tsx
+++ src/useInput.tsx
@@ -16,4 +16,4 @@ export const useInput = (initVal:any, validator:any) => {
         }
     }
     return { value, onChange };
-}
+};
