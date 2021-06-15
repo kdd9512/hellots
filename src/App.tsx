3333dd1e28f5e94961bf425da78003e64d1f965b
kdd9512 <hyunwoo8805@gmail.com>
2021-06-15 17:34:51 +0900

    20210614 commit07
    React Hooks 05(END)

--- src/App.tsx
+++ src/App.tsx
@@ -198,6 +198,40 @@ const useNetwork = (onChange: any) => {
     return status;
 };
 
+const useScroll = () => {
+    const [state, setState] = useState({
+        x: 0,
+        y: 0
+    });
+    const onScroll = () => {
+        setState({y: window.scrollY, x: window.scrollX});
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", onScroll)
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+    return state;
+};
+
+const useFullScreen = (callback:any) => {
+    const element: any = useRef();
+    const triggerFull = () => {
+        if (element.current) {
+            element.current.requestFullscreen();
+            if (callback && typeof callback === "function") {
+                callback(true)
+            }
+        }
+    };
+    const exitFull = () => {
+        document.exitFullscreen();
+        if (callback && typeof callback === "function") {
+            callback(false)
+        }
+    }
+    return {element, triggerFull, exitFull};
+}
+
 
 const App = () => {
     //
@@ -243,8 +277,15 @@ const App = () => {
     // }
     // const onLine = useNetwork(handleNetworkChange);
 
+    // const { y } = useScroll();
+
+    const onFullSc = (isFull:boolean) => {
+        console.log(isFull ? "Full" : "Non")
+    }
+    const {element, triggerFull, exitFull} = useFullScreen(onFullSc);
+
     return (
-        <div className="App">
+        <div className="App" style={{height: "1000vh"}}>
             {/*{count}*/}
             {/*<button onClick={() => setCount(count + 1)}> + </button>*/}
             {/*<button onClick={() => setCount(count - 1)}> - </button>*/}
@@ -270,6 +311,14 @@ const App = () => {
             {/*<p {...fadeInP}>asdfsadfasdfsadfsadfsadfsadfs</p>*/}
 
             {/*<h1>{onLine ? "online" : "offline"}</h1>*/}
+
+            {/*<h1 style={{position:"fixed", color: y > 100? "red" : "blue"}}>Hello</h1>*/}
+
+            <div ref={element}>
+                <img src="https://t1.daumcdn.net/cfile/tistory/996E76485C1F70711B"/>
+                <button onClick={exitFull}>exit fullscreen</button>
+            </div>
+            <button onClick={triggerFull}>make fullscreen</button>
 
 
         </div>
